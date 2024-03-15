d349576a80c4542e711df8d708463d93daeb9637
Ge6ben <dl22gesh40@gmail.com>
2024-03-15 16:59:43 +0300

    feat:create a basic table

---
+++ src/components/ag-grid-table.vue
@@ -0,0 +1,20 @@
+<template>
+     <!-- The AG Grid component -->
+    <ag-grid-vue
+      :rowData="props.rowData"
+      :columnDefs="props.colDefs"
+      style="  height: calc(100vh - 290px)"
+      class="ag-theme-quartz"
+    >
+    </ag-grid-vue>
+
+ </template>
+<script setup>
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import { AgGridVue } from "ag-grid-vue3"; // AG Grid Component
+const props = defineProps(['rowData' ,'colDefs'])
+
+
+</script>
+
