c2cd10ae275d2ebde7b037a380d2471bfa0ae732
Ge6ben <dl22gesh40@gmail.com>
2024-03-15 22:52:11 +0300

    feat:update app regarding table to support  SSRM

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,75 @@
 <template>
   <div>
-    <AGGridTable :colDefs="colDefs"  :rowData="rowData"></AGGridTable>
+    <AGGridTable :colDefs="colDefs" @on-grid-ready="onGridReady" />
   </div>
 </template>
 <script setup>
-import 'ag-grid-enterprise';
-import { LicenseManager } from 'ag-grid-enterprise';
- import AGGridTable from './components/ag-grid-table.vue'
+import 'ag-grid-enterprise'
+import { LicenseManager } from 'ag-grid-enterprise'
+import AGGridTable from './components/ag-grid-table.vue'
 import { onBeforeMount, ref } from 'vue'
-onBeforeMount(()=>{
-  LicenseManager.setLicenseKey(import.meta.env.AG_GRID_LICENSE_KEY  );
+
+onBeforeMount(() => {
+  LicenseManager.setLicenseKey(import.meta.env.AG_GRID_LICENSE_KEY)
 })
 
+const startRow = ref(0)
+const endRow = ref(1000)
+let total = ref(0)
+const gridApi = ref()
 
-const rowData = ref([
-  { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-  { make: "Ford", model: "F-Series", price: 33850, electric: false },
-  { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-]);
+const onGridReady = async (params) => {
+  gridApi.value = params.api
+  const datasource = createServerSideDatasource()
+  await gridApi.value.setGridOption('serverSideDatasource', datasource)
+}
+
+function createServerSideDatasource() {
+  return {
+    getRows: async (params) => {
+      gridApi.value.hideOverlay()
+
+      startRow.value = params.request.startRow
+      endRow.value = params.request.endRow
+
+      fetchData(startRow.value, endRow.value)
+        .then((data) => {
+          console.log('Fetch', startRow.value, endRow.value)
+          params.success({ rowData: data, rowCount: total })
+        })
+        .catch(() => {
+          params.fail()
+        })
+    }
+  }
+}
+
+function fetchData(startRow, endRow) {
+  return new Promise((resolve, reject) => {
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => {
+        data = data.map((i, index) => {
+          return { ...i, id: index }
+        })
+        total = data.length
+        const dataWithApplyPagination = data.slice(startRow, endRow)
+        resolve(dataWithApplyPagination)
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
 
 const colDefs = ref([
-  { field: "make" },
-  { field: "model" },
-  { field: "price" },
-  { field: "electric" }
-]);
-
- </script>
-
+  { field: 'id', headerName: 'Number' },
+  { field: 'athlete' },
+  { field: 'country' },
+  { field: 'year' },
+  { field: 'sport' },
+  { field: 'gold' },
+  { field: 'silver' },
+  { field: 'bronze' }
+])
+</script>
